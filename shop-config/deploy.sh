ce4c6f031f825964a8e65000aff14d43ca951a22
WallyS02 <sebkutny@gmail.com>
2024-01-12 22:17:24 +0100

    deploy.sh fix

--- shop-config/deploy.sh
+++ shop-config/deploy.sh
@@ -6,4 +6,6 @@ docker pull $IMAGE_NAME
 
 wget $COMPOSE_URL -O docker-compose-prod.yml
 
+sed -i -e 's/\r$//' ./docker-compose-prod.yml
+
 docker stack deploy -c docker-compose-prod.yml $STACK_NAME --with-registry-auth
